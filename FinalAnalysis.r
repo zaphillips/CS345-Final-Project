adac589ae5aaba1050db87666a1173ae3d9b0a0b
Zach <zphillips080@anselm.edu>
2024-04-30 15:51:29 -0400

    Co-authored-by: Anthony Grieco <agrieco20@users.noreply.github.com>

--- FinalAnalysis.r
+++ FinalAnalysis.r
@@ -1,8 +1,6 @@
-
 library(tidyverse)
 library(cluster)
 library(factoextra)
-library(dendextend)
 library(ggplot2)
 
 #----------------------------Prepare Data------------------------------------
@@ -73,7 +71,13 @@ receivingGrouped <- receivingGrouped %>%
 receivingGrouped_stn <- receivingGrouped
 receivingGrouped_stn[, 4:16] <- scale(receivingGrouped_stn[, 4:16])
 
-#Prepare subsets of data
+#Move Columns
+receivingGrouped_stn <- receivingGrouped_stn %>%
+  relocate(YardsPerReception, .before = Lng) %>%
+  relocate(Yds, .before = YardsPerReception) %>%
+  relocate(Receptions, .before = Yds)
+
+#Prepare subsets of data (for each position)
 wr_stats <- receivingGrouped_stn %>%
   filter(Position == "WR") %>%
   select(-Position)
@@ -95,30 +99,30 @@ receiving_label <- receivingGrouped_stn$Position
 receivingGrouped_stn <- receivingGrouped_stn %>%
   select(-Position)
 
-#Move Columns
-receivingGrouped_stn <- receivingGrouped_stn %>%
-  relocate(YardsPerReception, .before = Lng) %>%
-  relocate(Yds, .before = YardsPerReception) %>%
-  relocate(Receptions, .before = Yds)
-
 #-------------------Predicting Position Based on Receiving Stats (2014-2023)---------------------------
+#Seed
 set.seed(1234)
 
+#kmeans
 receivingCluster <- kmeans(receivingGrouped_stn[, 8:14], center=4, nstart=25)
 receivingCluster
 
+#Compare with label
 table(receivingCluster$cluster, receiving_label)
 
+#Determine number of clusters
 fviz_nbclust(receivingGrouped_stn[, 8:14], kmeans, method="wss")
 fviz_nbclust(receivingGrouped_stn[, 8:14], kmeans, method="silhouette")
 
 gap_stat <- clusGap(receivingGrouped_stn[, 8:14], FUN = kmeans, nstart=25, K.max = 10, B=50)
 fviz_gap_stat(gap_stat)
 
+#Final kmeans
 km.res <- kmeans(receivingGrouped_stn[8:14], 4, nstart = 25)
 km.res
 fviz_cluster(km.res, data=receivingGrouped_stn[8:14])
 
+#Create dataframe including predictions from model
 positionPrediction <- receivingGrouped_stn
 positionPrediction$cluster <- as.factor(km.res$cluster)
 
@@ -126,11 +130,141 @@ summary(receivingGrouped_stn)
 
 positionPrediction$actual_position <- receiving_label
 
+#Relocate columns
 positionPrediction <- positionPrediction %>%
   relocate(actual_position, .before = Player) %>%
   relocate(cluster, .after = actual_position)
 
+#Create confusion matrix and calculate accuracy of model
 cfm <- table(positionPrediction$cluster, receiving_label)
 cfm
 acc <- sum(diag(cfm)) / sum(rowSums(cfm)) * 100
 paste("Accuracy: ", round(acc, 2))
+
+#-------------------Identifying Additional Clusters Based on Receiving Stats (2014-2023)---------------------------
+
+# Expected Sub Positions:
+# RB: Receiving, Hybrid, Power
+# WR: Boundary, Move, Slot
+# TE: Inline, Move
+# FB: Receiving
+
+#Create kmeans based on expected number of clusters (based on expected sub-positions)
+receivingCluster2 <- kmeans(receivingGrouped_stn[, 8:14], center=9, nstart=25)
+receivingCluster2
+
+#Final kmeans
+km.res2 <- kmeans(receivingGrouped_stn[8:14], 9, nstart = 25)
+km.res2
+fviz_cluster(km.res2, data=receivingGrouped_stn[8:14])
+
+#Compare similar players (based on model)
+positionPrediction2 <- receivingGrouped_stn
+positionPrediction2$cluster <- as.factor(km.res2$cluster)
+
+#Move cluster column and arrange data by cluster
+positionPrediction2 <- positionPrediction %>%
+  relocate(cluster, .after = Player) %>%
+  arrange(cluster)
+
+View(positionPrediction2)
+
+#-------------------Identifying Sub-Positions Based on Receiving Stats (2014-2023)---------------------------
+
+# Expected Sub Positions:
+# RB: Receiving, Hybrid, Power
+# WR: Boundary, Move, Slot
+# TE: Inline, Move
+# FB: Receiving
+
+#RBs:
+rbCluster <- kmeans(rb_stats[, 8:14], center=3, nstart=25)
+rbCluster
+
+fviz_nbclust(rb_stats[, 8:14], kmeans, method="wss")
+fviz_nbclust(rb_stats[, 8:14], kmeans, method="silhouette")
+
+gap_stat_rb <- clusGap(rb_stats[, 8:14], FUN = kmeans, nstart=25, K.max = 10, B=50)
+fviz_gap_stat(gap_stat_rb)
+
+km.res_rb <- kmeans(rb_stats[8:14], 3, nstart = 25)
+km.res_rb
+fviz_cluster(km.res_rb, data=rb_stats[8:14])
+
+#Compare similar players (based on model)
+positionPrediction_rb <- rb_stats
+positionPrediction_rb$cluster <- as.factor(km.res_rb$cluster)
+
+#Move cluster column and arrange data by cluster
+positionPrediction_rb <- positionPrediction_rb %>%
+  relocate(cluster, .after = Player) %>%
+  arrange(cluster)
+
+View(positionPrediction_rb)
+
+#WRs:
+wrCluster <- kmeans(wr_stats[, 8:14], center=3, nstart=25)
+wrCluster
+
+fviz_nbclust(wr_stats[, 8:14], kmeans, method="wss")
+fviz_nbclust(wr_stats[, 8:14], kmeans, method="silhouette")
+
+gap_stat_wr <- clusGap(wr_stats[, 8:14], FUN = kmeans, nstart=25, K.max = 10, B=50)
+fviz_gap_stat(gap_stat_wr)
+
+km.res_wr <- kmeans(wr_stats[8:14], 3, nstart = 25)
+km.res_wr
+fviz_cluster(km.res_wr, data=wr_stats[8:14])
+
+#Compare similar players (based on model)
+positionPrediction_wr <- wr_stats
+positionPrediction_wr$cluster <- as.factor(km.res_wr$cluster)
+
+#Move cluster column and arrange data by cluster
+positionPrediction_wr <- positionPrediction_wr %>%
+  relocate(cluster, .after = Player) %>%
+  arrange(cluster)
+
+View(positionPrediction_wr)
+
+#TEs:
+teCluster <- kmeans(te_stats[, 8:14], center=2, nstart=25)
+teCluster
+
+fviz_nbclust(te_stats[, 8:14], kmeans, method="wss")
+fviz_nbclust(te_stats[, 8:14], kmeans, method="silhouette")
+
+gap_stat_te <- clusGap(te_stats[, 8:14], FUN = kmeans, nstart=25, K.max = 10, B=50)
+fviz_gap_stat(gap_stat_te)
+
+km.res_te <- kmeans(te_stats[8:14], 2, nstart = 25)
+km.res_te
+fviz_cluster(km.res_te, data=te_stats[8:14])
+
+#Compare similar players (based on model)
+positionPrediction_te <- te_stats
+positionPrediction_te$cluster <- as.factor(km.res_te$cluster)
+
+#Move cluster column and arrange data by cluster
+positionPrediction_te <- positionPrediction_te %>%
+  relocate(cluster, .after = Player) %>%
+  arrange(cluster)
+
+View(positionPrediction_te)
+
+#FBs:
+
+#NOT ENOUGH DATA TO RUN ANALYSIS (Commented Out Code to Prevent Errors)
+
+# fbCluster <- kmeans(fb_stats[, 8:14], center=2, nstart=25)
+# fbCluster
+
+# fviz_nbclust(fb_stats[, 8:14], kmeans, method="wss")
+# fviz_nbclust(fb_stats[, 8:14], kmeans, method="silhouette")
+
+# gap_stat_fb <- clusGap(fb_stats[, 8:14], FUN = kmeans, nstart=25, K.max = 10, B=50)
+# fviz_gap_stat(gap_stat_fb)
+
+# km.res_fb <- kmeans(fb_stats[8:14], 2, nstart = 25)
+# km.res_fb
+# fviz_cluster(km.res_fb, data=fb_stats[8:14])
